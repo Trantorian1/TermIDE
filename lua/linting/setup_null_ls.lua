723697adb4c3e599dbf63664f2f4db8d7b0543a9
emcnab <eliot.code@proton.me>
2023-04-26 14:49:21 +0200

    feat(linting): null-ls conditonal activation
    Null-ls no longuer activates for certain file names. Currently this is
    limited to test files and disables norminette highlighting for unit
    tests.

--- lua/linting/setup_null_ls.lua
+++ lua/linting/setup_null_ls.lua
@@ -10,7 +10,17 @@ if not plugins.linting.null_ls ["loaded"]then
 	return
 end
 
+-- disables null_ls for test files
+local function should_attach(bufnr)
+	local bufname = vim.api.nvim_buf_get_name(bufnr)
+	if (string.match(bufname, 'test.*')) then
+		return false
+	end
+	return true
+end
+
 -- null-ls setup
 plugins.linting.null_ls.setup {
-	debounce = 10
+	debounce = 10,
+	should_attach = should_attach
 }
